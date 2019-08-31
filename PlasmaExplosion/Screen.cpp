1ffa1cb16ee419bc18c95951603a72362d63dcd3
Sergei Kutikin <sergei.kutikin@outlook.com>
2019-08-31 14:57:45 +0200

    first OOP Screen implementation

---
+++ PlasmaExplosion/Screen.cpp
@@ -0,0 +1,90 @@
+#include "Screen.h"
+#include <iostream>
+
+using namespace std;
+
+namespace ssk {
+	Screen::Screen(): m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer(NULL)
+	{
+	}
+
+	bool Screen::init()
+	{
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+			cout << "ssk::Screen: SDL init failed" << endl;
+			return false;
+		}
+
+		cout << "ssk::Screen: SDL init succeeded" << endl;
+
+		m_window = SDL_CreateWindow(
+			"Plasma Explosion",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN
+		);
+
+		if (m_window == NULL) {
+			cout << "ssk::Screen: Error initalizing the window: " << SDL_GetError() << endl;
+			SDL_Quit();
+			return false;
+		}
+
+		m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
+		m_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		if (m_renderer == NULL) {
+			cout << "ssk::Screen: Error initalizing the renderer: " << SDL_GetError() << endl;
+			SDL_DestroyWindow(m_window);
+			SDL_Quit();
+			return false;
+		}
+
+		if (m_texture == NULL) {
+			cout << "ssk::Screen: Error initalizing the texture: " << SDL_GetError() << endl;
+			SDL_DestroyWindow(m_window);
+			SDL_DestroyRenderer(m_renderer);
+			SDL_Quit();
+			return false;
+		}
+
+		m_buffer = new Uint32[SCREEN_HEIGHT * SCREEN_WIDTH];
+
+		memset(m_buffer, 0x00, SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(Uint32));
+
+		for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
+			m_buffer[i] = 0xFF8080FF;
+		}
+
+		SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+		SDL_RenderClear(m_renderer);
+		SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+		SDL_RenderPresent(m_renderer);
+
+		return true;
+	}
+
+	bool Screen::processEvents()
+	{
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	void Screen::close()
+	{
+		delete[] m_buffer;
+		SDL_DestroyWindow(m_window);
+		SDL_DestroyRenderer(m_renderer);
+		SDL_DestroyTexture(m_texture);
+
+		SDL_Quit();
+	}
+}
