a06cecd1a23272577b2b86c464f645d5ec145a71
Sergei Kutikin <sergei.kutikin@outlook.com>
2019-08-31 14:11:00 +0200

    basic color manipulation

--- PlasmaExplosion/PlasmaExplosion.cpp
+++ PlasmaExplosion/PlasmaExplosion.cpp
@@ -29,6 +29,33 @@ int main(int argc, char *args[]) {
 		return 2;
 	}
 
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if (renderer == NULL) {
+		cout << "Error initalizing the renderer: " << SDL_GetError() << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if (texture == NULL) {
+		cout << "Error initalizing the texture: " << SDL_GetError() << endl;
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(renderer);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32* buffer = new Uint32[SCREEN_HEIGHT*SCREEN_WIDTH];
+
+	memset(buffer, 0x00, SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
 	bool quit = false;
 
 	SDL_Event event;
