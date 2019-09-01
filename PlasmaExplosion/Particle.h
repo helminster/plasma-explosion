a0c014cb21ba719a380e9d08a2ddd0a136e7e35d
Sergei Kutikin <sergei.kutikin@outlook.com>
2019-09-01 13:59:17 +0200

    Implement round explosion animation

--- PlasmaExplosion/Particle.h
+++ PlasmaExplosion/Particle.h
@@ -5,8 +5,9 @@ namespace ssk {
 	{
 		double m_x;
 		double m_y;
-		double m_xspeed;
-		double m_yspeed;
+
+		double m_speed;
+		double m_direction;
 	public:
 		Particle();
 		~Particle();
--- PlasmaExplosion/PlasmaExplosion.cpp
+++ PlasmaExplosion/PlasmaExplosion.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *args[]) {
 			Particle particle = pParticles[i];
 
 			int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
-			int y = (particle.m_y + 1) * Screen::SCREEN_HEIGHT / 2;
+			int y = particle.m_y * Screen::SCREEN_WIDTH / 2 + Screen::SCREEN_HEIGHT / 2;
 
 			screen.setPixel(x, y, red, green, blue);
 		}
